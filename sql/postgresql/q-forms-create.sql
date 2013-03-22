a36aacd3c8e5a73252bfe6100ee9ad7f8094c53e
Benjamin Brink <kappa@dekka.com>
2013-03-22 08:10:28 +0000

    data model adjustments

--- sql/postgresql/q-forms-create.sql
+++ sql/postgresql/q-forms-create.sql
@@ -5,20 +5,23 @@
 -- @cvs-id
 --
 
--- The purpose of this table is to answer the question:
+-- The purpose of table qf_key_map is to answer the question:
 --   How do we know the posted data is directly from a form presented to a user, 
---   and not the result of some kind of serrepitious form post?
+--   and not the result of some kind of surrepitious form post?
 -- By logging somewhat unique aspects of a form rendering transaction,
 -- and checking them against form post values.
--- Form rendering uniqueness includes: location (form action), time rendered, session_id, and port.
--- Port refers to http vs. https.
+
+-- Form rendering uniqueness includes: location (form action), time rendered, session_id, and port (ssl y/n)
+
 -- Using a random key to reference the data points where we can filter the form entry by the key
 -- and subsequently check its data point values, ignoring any case where the key doesn't fit.
+
 -- For random_key, we are using a local version of sec_random_token
 --  from packages/acs-tcl/tcl/security-procs.tcl
 -- For general security session notes, see /doc/security-design.html
-CREATE TABLE qf_form_key_map (
-       key_id,
+CREATE TABLE qf_key_map (
+       instance_id integer,
+       key_id numeric,
        session_id varchar(100),
        -- this should be ns_conn url
        action_url varchar(300),
@@ -29,8 +32,13 @@ CREATE TABLE qf_form_key_map (
        -- session_id resides as a cookie so is hackable,
        -- whereas client_ip is not
        client_ip varchar(30),
-       sec_hash varchar(40)
+       sec_hash varchar(40),
+       -- if avail_p is 0, then the form has already been posted
+       -- and the key is no longer available.
+       avail_p integer default '1'
 );
+--  Set up a scheduled proc to regularly clean up where avail_p = 0
+
 -- The standard is to not save the data point values, just a hash of them.
 -- That makes it more difficult to reverse hack the form state even with access to the db.
 -- ie an action_url couldn't be used to reduce the possilbe matches in the table.
