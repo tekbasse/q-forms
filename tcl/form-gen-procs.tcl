138de2f0d09f9093d51d7ae3c383a56bc93a0470
Benjamin Brink <tekbasse@yahoo.com>
2017-11-26 17:07:26 -0500

    add more notes

--- tcl/form-gen-procs.tcl
+++ tcl/form-gen-procs.tcl
@@ -1,7 +1,8 @@
 ad_library {
 
     routines for creating, managing customizable forms
-    for adapting package applications to site specific requirements.
+    for adapting package applications to site specific requirements
+    by adding additional fields aka object attributes.
     @creation-date 24 Nov 2017
     @Copyright (c) 2017 Benjamin Brink
     @license GNU General Public License 2, see project home or http://www.gnu.org/licenses/gpl.html
@@ -11,11 +12,20 @@ ad_library {
 }
 
 #agenda:
-# qf_form_prepare form_id
+# qf_form_prepare form_id form_fields_arr
 #      Prepares an lists_array definition of a form
+#      Grabs custom definitions
+#      Grabs/overwrites customs with package defaults
+#         to force package specific requirements
 #      This way, can check if a package_id has a parameter enableFormGenP
 #      If enableFormGenP and apm_package_enabled_p spreadsheet
 #      Then do integration business logic
+
+# qf_form_fields 
+#      returns list of default form fields + plus any custom ones
+
+# qf_form_array_read (as name/val list pairs)
+#      reads data from tips_ database that match form_array's unique_key
 
 #qf_form_generate_html4 form_id
 # converts prepared list_array to html
@@ -26,3 +36,7 @@ ad_library {
 #qf_form_generate_xml_v001 form_id
 # converts prepared list_array to xml (mainly for saas)
 
+# object?? what other word works instead of 'form' that doesn't conflict with
+# existing openacs-isms
+#qf_object_generate_html4 arrayname
+#qf_object_generate_html5 arrayname
