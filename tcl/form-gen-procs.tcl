cf237d9ac8a3a4da0bd9843033a67c5cf66f0531
Benjamin Brink <tekbasse@yahoo.com>
2017-11-27 02:11:56 -0500

    add more strategy notes

--- tcl/form-gen-procs.tcl
+++ tcl/form-gen-procs.tcl
@@ -31,7 +31,11 @@ ad_library {
 #           for defaults provided by q-forms only. 
 #           So, q-forms must have its own qfo datatypes, yet a UI is 
 #           needed to dynamically change this, and spreadsheet shouldn't require
-#           q-forms.. so, create package qfo that requires spreadsheet and q-forms.
+#           q-forms.. so, create package qfo that requires spreadsheet and q-forms?
+#           qfo shouldn't require spreadsheet. Solution is to have
+#           qfo package require q-forms and a q-data-types package, and
+#           spreadsheet requires q-data-types package.
+#           spreadsheet is only required if extended qfo package features.
 #           default values (set in context of qf_input_as_array)
 #      This way, can check if a package_id has a parameter enableFormGenP
 #      If enableFormGenP and apm_package_enabled_p spreadsheet
