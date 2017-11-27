2f2e8787c06c8e168e3c74da3260808d6bddfbd3
Benjamin Brink <tekbasse@yahoo.com>
2017-11-27 00:17:34 -0500

    add more notes

--- tcl/form-gen-procs.tcl
+++ tcl/form-gen-procs.tcl
@@ -14,11 +14,25 @@ ad_library {
 #agenda:
 # qfo = q-form object
 
-# qfo_prepare form_id form_fields_arr
-#      Prepares an lists_array definition of a form
-#      Grabs custom definitions
+# qfo_prepare form_id form_fields_larr
+#      Prepares a lists_array definition of a form
+
+#      Grabs custom definitions in context of tips_ api
+
 #      Grabs/overwrites customs with package defaults
-#         to force package specific requirements
+#         to force package specific requirements.
+#         Customization adds fields, doesn't change supplied ones.
+#         Package defaults need to be defined using simple,
+#         largely self-explanatory code
+#         What is needed?  field attributes for qf_input or qf_textarea
+#           additional fields: data type for validation and view.
+#           Attributes could default to ones provided by datatype.
+#           datatype refers to qss_tips_data_types.. except that doesn't work
+#           for defaults provided by q-forms only. 
+#           So, q-forms must have its own qfo datatypes, yet a UI is 
+#           needed to dynamically change this, and spreadsheet shouldn't require
+#           q-forms.. so, create package qfo that requires spreadsheet and q-forms.
+#           default values (set in context of qf_input_as_array)
 #      This way, can check if a package_id has a parameter enableFormGenP
 #      If enableFormGenP and apm_package_enabled_p spreadsheet
 #      Then do integration business logic
