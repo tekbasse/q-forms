4629dae69ba92ae22a897894a012127bace34587
Benjamin Brink <tekbasse@yahoo.com>
2018-01-03 18:54:54 -0500

    add testing for form generating

---
+++ tcl/test/q-forms-gen-procs.tcl
@@ -0,0 +1,26 @@
+ad_library {
+    Automated tests for q-forms
+    @creation-date 2017-04-09
+}
+
+aa_register_case -cats {api smoke} qf_form_gen_checks {
+    Test form generation features
+} {
+    aa_run_with_teardown \
+        -test_code {
+            #         -rollback \
+            ns_log Notice "qf_form_gen_checks.12: Begin test"
+
+            set x [qfo_gen -test_p2 -5 -test_param test]
+
+        } 
+    # example code
+    # -teardown_code {
+    # 
+    #acs_user::delete -user_id $user1_arr(user_id) -permanent
+    
+    # }
+    #aa_true "Test for .." $passed_p
+    #aa_equals "Test for .." $test_value $expected_value
+}    
+
